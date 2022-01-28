77b5a6042ced6145a9e68b4320c68c7688566c10
Diego Piovezana <diego.g.piovezana@accenture.com>
2022-01-28 15:54:59 -0300

    refactor: pequenos ajustes em tratativas de exceções

--- SheetHelper/ExcelHelper.cs
+++ SheetHelper/ExcelHelper.cs
@@ -51,6 +51,9 @@ namespace SheetHelper
             return columnsASCII;
         }
 
+        /// <summary>
+        /// Realiza a leitura de arquivos .xls, .xlsx e .xlsb
+        /// </summary>
         private static DataSet ReadXLS(FileStream stream)
         {
             // Formato de detecção automática, suporta: 
@@ -68,6 +71,9 @@ namespace SheetHelper
             }
         }
 
+        /// <summary>
+        /// Realiza a leitura de arquivos .csv
+        /// </summary>
         private static DataSet ReadCSV(FileStream stream)
         {
             // Realiza a leitura do arquivo Excel CSV
@@ -81,10 +87,6 @@ namespace SheetHelper
 
 
 
-
-
-
-        #region Summary
         /// <summary>
         /// Realiza a conversão do arquivo Excel localizado em <paramref name="origin"/>, salva em <paramref name="destiny"/>
         /// e retorna 'true' caso a conversão tenha ocorrido com sucesso.
@@ -97,7 +99,6 @@ namespace SheetHelper
         /// <param name="header">Tamanho do cabeçalho a ser retirado (0 para manter a primeira linha). Ex.: 1</param>
         /// <param name="columns">"Vetor de caracteres (maiúsculo ou minúsculo) contendo todas as colunas desejadas. Ex.: "{ 'A', 'b', 'C', 'E' }"</param>
         /// <returns>"true" se convertido com sucesso. "false" se não convertido.</returns>
-        #endregion
         public static bool Converter(string origin, string destiny, int sheet, string separator, int header, string[] columns)
         {
 
@@ -186,9 +187,6 @@ namespace SheetHelper
         }
 
 
-
-
-
         /// <summary>
         /// Realiza a conversão do arquivo Excel localizado em <paramref name="origin"/>, salva em <paramref name="destiny"/>
         /// com tratativa de exceçoes para o usuário final (arquivo inexistente no diretorio ou aberto durante a conversão)
@@ -213,79 +211,81 @@ namespace SheetHelper
                 return Converter(origin, destiny, sheet, separator, header, columns);
             }
 
-            catch (Exception e)
+
+            #region Se arquivo nao localizado            
+            catch (FileNotFoundException fnfEx) when (fnfEx.ToString().Contains("not find file") || fnfEx.ToString().Contains("arquivo não encontrado"))
             {
+                var result3 = MessageBox.Show(
+                                   "O arquivo '" + Path.GetFileName(origin) + "' não foi localizado. Por favor, verifique se o arquivo está presente no repositório de origem e confirme para continuar: "
+                                   + "\n\n" + origin,
+                                   "Aviso",
+                                   MessageBoxButtons.OKCancel,
+                                   MessageBoxIcon.Exclamation);
 
-                switch (e.ToString())
+
+                if (result3 == DialogResult.OK)
                 {
-                    case string a when a.Contains("being used by another process"): // Se arquivo esta em uso
+                    goto again; // Tenta realizar a conversão novamente
+                }
 
-                        countOpen++; // Contador de tentativas com falha de arquivo aberto
+                return false;
+            }
+            #endregion
 
-                        if (countOpen >= 2) // Se necessario forçar o fechamento do Excel (a partir do 2 caso)
-                        {
+            #region Se arquivo esta em uso
+            catch (IOException eiEx) when (eiEx.ToString().Contains("being used by another process") || eiEx.ToString().Contains("sendo usado por outro processo"))
+            {
+                countOpen++; // Contador de tentativas com falha de arquivo aberto
 
-                            var result1 = MessageBox.Show(
-                               "Parece que o arquivo ainda continua em uso. Deseja forçar o encerramento do Excel e tentar novamente? \n\nAs alterações não serão salvas.",
-                               "Aviso",
-                               MessageBoxButtons.YesNo,
-                               MessageBoxIcon.Exclamation);
+                if (countOpen >= 2) // Se necessario forçar o fechamento do Excel (a partir do 2 caso)
+                {
 
-                            if (result1 == DialogResult.Yes)
-                            {
-                                CloseExcel(); // Encerra todos os processos do Excel
-                                Thread.Sleep(1500); // Aguarda o excel fechar por completo durante 1,5 segundo
-                                goto again; // Tenta realizar a conversão novamente
+                    var result1 = MessageBox.Show(
+                       "Parece que o arquivo ainda continua em uso. Deseja forçar o encerramento do Excel e tentar novamente? \n\nAs alterações não serão salvas.",
+                       "Aviso",
+                       MessageBoxButtons.YesNo,
+                       MessageBoxIcon.Exclamation);
 
-                            } // Se No, continua execucao abaixo
+                    if (result1 == DialogResult.Yes)
+                    {
+                        CloseExcel(); // Encerra todos os processos do Excel
+                        Thread.Sleep(1500); // Aguarda o excel fechar por completo durante 1,5 segundo
+                        goto again; // Tenta realizar a conversão novamente
 
-                        }
-
-                        var result2 = MessageBox.Show(
-                            "O arquivo '" + Path.GetFileName(origin) + "' ou '" + Path.GetFileName(destiny) + "' está sendo utilizado em outro processo. Por favor, finalize seu uso e em seguida confirme para continuar.",
-                            "Aviso",
-                            MessageBoxButtons.OKCancel,
-                            MessageBoxIcon.Error);
-
-
-                        if (result2 == DialogResult.OK)
-                        {
-                            goto again; // Tenta realizar a conversão novamente
-                        }
-                        else // Se cancelar
-                        {
-                            return false;
-                        }
-
-
-                    case string b when b.Contains("not find file"): // Se arquivo nao localizado
-
-                        var result3 = MessageBox.Show(
-                               "O arquivo '" + Path.GetFileName(origin) + "' não foi localizado. Por favor, verifique se o arquivo está presente no repositório de origem e confirme para continuar: "
-                               + "\n\n" + origin,
-                               "Aviso",
-                               MessageBoxButtons.OKCancel,
-                               MessageBoxIcon.Exclamation);
-
-
-                        if (result3 == DialogResult.OK)
-                        {
-                            goto again; // Tenta realizar a conversão novamente
-                        }
-
-                        return false;
-
-
-                    case string b when b.Contains("Invalid file signature"): // Se arquivo em formato não suportado
-                        throw new Exception($"Erro! Sem suporte para converter arquivos de origem '{Path.GetExtension(origin)}'.");
-
-
-                    default: // Para outros tipos de Exception
-
-                        throw e;
+                    } // Se No, continua execucao abaixo
 
                 }
+
+                var result2 = MessageBox.Show(
+                    "O arquivo '" + Path.GetFileName(origin) + "' ou '" + Path.GetFileName(destiny) + "' está sendo utilizado em outro processo. Por favor, finalize seu uso e em seguida confirme para continuar.",
+                    "Aviso",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Error);
+
+
+                if (result2 == DialogResult.OK)
+                {
+                    goto again; // Tenta realizar a conversão novamente
+                }
+                else // Se cancelar
+                {
+                    return false;
+                }
+
+
             }
+
+            #endregion
+
+            #region Se arquivo em formato não suportado
+            catch (ExcelDataReader.Exceptions.HeaderException heEx) when (heEx.ToString().Contains("Invalid file signature") || heEx.ToString().Contains("Assinatura de arquivo inválida"))
+            {
+                throw new Exception($"Erro! Sem suporte para converter arquivos de origem '{Path.GetExtension(origin)}'.");
+
+            }
+            #endregion
+
+
         }
     }
 }
