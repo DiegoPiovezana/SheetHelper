e45832942ba1a52412014b83652e8a4d282b6bbb
diegopiovezana <diegopiovezana@gmail.com>
2024-03-20 23:09:33 -0300

    refactor: organize exception messages

--- SheetHelper/Globalization/Messages.cs
+++ SheetHelper/Globalization/Messages.cs
@@ -1,12 +1,12 @@
-﻿using ExcelDataReader.Core;
-using System.Data.Common;
-using System.Globalization;
+﻿using System.Globalization;
 using System.IO;
 
 namespace SH.Globalization
 {
     internal static class Messages
     {
+        #region Warning
+
         internal static string Warning()
         {
             return CultureInfo.CurrentCulture.Name switch
@@ -14,25 +14,61 @@ namespace SH.Globalization
                 "pt-BR" => $"Aviso",
                 _ => $"Warning",
             };
-        }       
+        }
 
-        internal static string SheetErrorIndex(string sheet)
+        internal static string VisitDocExceptions()
         {
             return CultureInfo.CurrentCulture.Name switch
             {
-                "pt-BR" => $"Erro ao selecionar a aba desejada! Verifique se o índice da aba está correto.",
-                _ => $"Error selecting the desired sheet! Please check if the sheet index is correct.",
+                "pt-BR" => $"Por favor, visite https://bit.ly/SheetHelper_Exceptions para saber mais.",
+                _ => $"Please visit https://bit.ly/SheetHelper_Exceptions to learn more.",
             };
         }
 
-        internal static string SheetNameNotFind(string sheet)
+
+        #endregion
+
+        #region Sheet
+
+        internal static string SheetErrorIndex(int sheetIndex)
         {
             return CultureInfo.CurrentCulture.Name switch
             {
-                "pt-BR" => $"Não foi possível encontrar a planilha desejada '{sheet}'! Verifique se o nome da planilha está correto.",
-                _ => $"Unable to find the desired sheet '{sheet}'! Please check if the sheet name is correct.",
+                "pt-BR" => $"Erro ao selecionar a aba desejada! Verifique se o índice '{sheetIndex}' da aba está correto.",
+                _ => $"Error selecting the desired sheet! Please check if the sheet index '{sheetIndex}' is correct.",
             };
         }
+
+        internal static string SheetNameNotFind(string sheetName)
+        {
+            return CultureInfo.CurrentCulture.Name switch
+            {
+                "pt-BR" => $"Não foi possível encontrar a planilha desejada '{sheetName}'! Verifique se o nome da planilha está correto.",
+                _ => $"Unable to find the desired sheet '{sheetName}'! Please check if the sheet name is correct.",
+            };
+        }
+
+        internal static string SheetFirstIndex()
+        {
+            return CultureInfo.CurrentCulture.Name switch
+            {
+                "pt-BR" => $"A primeira aba é 1!",
+                _ => $"The first sheet is 1!",
+            };
+        }
+
+        internal static string SheetNameInvalid(string sheetName)
+        {
+            return CultureInfo.CurrentCulture.Name switch
+            {
+                "pt-BR" => $"Nome de aba '{sheetName}' é inválido.",
+                _ => $"Invalid sheet name '{sheetName}'.",
+            };
+        }
+
+        #endregion
+
+        #region Column
 
         internal static string ColumnOutRange(string column, int limitIndexColumn, int indexColumn)
         {
@@ -60,6 +96,10 @@ namespace SH.Globalization
                 _ => $"The column '{column}' entered does not have a valid pattern",
             };
         }
+
+        #endregion
+
+        #region Row
 
         internal static string RowOutRange(string row, int limitIndexRows, int indexRow)
         {
@@ -97,6 +137,10 @@ namespace SH.Globalization
             };
         }
 
+        #endregion
+
+        #region Separator
+
         internal static string SeparatorInvalid(string separator)
         {
             return CultureInfo.CurrentCulture.Name switch
@@ -106,30 +150,16 @@ namespace SH.Globalization
             };
         }
 
-        internal static string SheetFirstIndex()
-        {
-            return CultureInfo.CurrentCulture.Name switch
-            {
-                "pt-BR" => $"A primeira aba é 1!",
-                _ => $"The first sheet is 1!",
-            };
-        }
+        #endregion
 
-        internal static string SheetNameInvalid(string sheetName)
-        {
-            return CultureInfo.CurrentCulture.Name switch
-            {
-                "pt-BR" => $"Nome de aba '{sheetName}' é inválido.",
-                _ => $"Invalid sheet name '{sheetName}'.",
-            };
-        }
+        #region File
 
         internal static string FileOriginInUse(string pathFile)
         {
             return CultureInfo.CurrentCulture.Name switch
             {
-                "pt-BR" => $"O arquivo de origem '{Path.GetFileName(pathFile)}' está sendo utilizado em outro processo. Por favor, finalize seu uso e em seguida tente novamente",
-                _ => $"The origin file '{Path.GetFileName(pathFile)}' is being used by another process. Please finish its use and then try again",
+                "pt-BR" => $"O arquivo de origem '{Path.GetFileName(pathFile)}' está sendo utilizado em outro processo. Por favor, finalize seu uso e em seguida tente novamente.",
+                _ => $"The origin file '{Path.GetFileName(pathFile)}' is being used by another process. Please finish its use and then try again.",
             };
         }
 
@@ -140,7 +170,7 @@ namespace SH.Globalization
                 "pt-BR" => $"Ocorreu um erro ao validar o arquivo de origem!",
                 _ => $"An error occurred while validating the origin file!",
             };
-        }         
+        }
 
         internal static string FileOriginNotFound(string pathFile)
         {
@@ -176,7 +206,7 @@ namespace SH.Globalization
                 "pt-BR" => $"Ocorreu um erro ao validar o caminho do arquivo de destino!",
                 _ => $"An error occurred while validating the destination file path!",
             };
-        }      
+        }
 
         internal static string FileDestinyNameNull()
         {
@@ -186,6 +216,18 @@ namespace SH.Globalization
                 _ => $"The destination path for the file ('destiny') is null or empty.",
             };
         }
+
+        internal static string FileZipUnable(string extension)
+        {
+            return CultureInfo.CurrentCulture.Name switch
+            {
+                "pt-BR" => $"Não é possível extrair este arquivo, pois '{extension}' ainda não é suportada.",
+                _ => $"Can't extract this file, because '{extension}' is not supported yet.",
+            };
+        }
+
+
+        #endregion
 
     }
 }
